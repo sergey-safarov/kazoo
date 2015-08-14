d4ceaba48d09a440763e88fe47c5a73b949187aa
James Aimonetti <james.aimonetti@gmail.com>
2015-08-14 10:56:16 -0700

    KAZOO-244: reconcile cascaded ip quantities

--- core/whistle_services-1.0.0/src/services/wh_service_ips.erl
+++ core/whistle_services-1.0.0/src/services/wh_service_ips.erl
@@ -7,9 +7,13 @@
 %%%-------------------------------------------------------------------
 -module(wh_service_ips).
 
--export([reconcile/1, reconcile/2]).
+-export([reconcile/1, reconcile/2
+         ,reconcile_cascade/2
+        ]).
 
 -include("../whistle_services.hrl").
+
+-define(CATEGORY, <<"ips">>).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -27,28 +31,57 @@ reconcile(Services) ->
             lager:debug("unable to get current dedicated ips in service: ~p", [_R]),
             Services;
         {'ok', JObjs} ->
-            S = wh_services:reset_category(<<"ips">>, Services),
-            wh_services:update(<<"ips">>, <<"dedicated">>, length(JObjs), S)
+            S = wh_services:reset_category(?CATEGORY, Services),
+            wh_services:update(?CATEGORY, <<"dedicated">>, length(JObjs), S)
     end.
 
 reconcile(Services, 'undefined') -> Services;
 reconcile(Services0, IpType) when is_binary(IpType) ->
     Services1 = reconcile(Services0),
-    Quantity = wh_services:updated_quantity(<<"ips">>, IpType, Services1),
-    wh_services:update(<<"ips">>, IpType, Quantity+1, Services1);
+    Quantity = wh_services:updated_quantity(?CATEGORY, IpType, Services1),
+    wh_services:update(?CATEGORY, IpType, Quantity+1, Services1);
 reconcile(Services, Props) ->
     lists:foldl(
-      fun reconcile_foldl/2
+      fun(IP, S) ->
+              reconcile_foldl(IP
+                              ,S
+                              ,fun wh_services:updated_quantity/3
+                              ,fun wh_services:update/4
+                             )
+      end
       ,reconcile(Services)
       ,Props
      ).
 
--spec reconcile_foldl({ne_binary(), integer() | ne_binary()}, wh_services:services()) ->
+reconcile_cascade(Services, 'undefined') -> Services;
+reconcile_cascade(Services, <<_/binary>> = IpType) ->
+    Quantity = wh_services:cascade_quantity(?CATEGORY, IpType, Services),
+    wh_services:update_cascade(?CATEGORY, IpType, Quantity+1);
+reconcile_cascade(Services, Props) ->
+    lists:foldl(
+      fun(IP, S) ->
+              reconcile_foldl(IP
+                              ,S
+                              ,fun wh_services:cascade_quantity/3
+                              ,fun wh_services:update_cascade/4
+                             )
+      end
+      ,reconcile(Services)
+      ,Props
+     ).
+
+-type ip_info() :: {ne_binary(), integer() | ne_binary()}.
+-type get_fun() :: fun((ne_binary(), ne_binary(), wh_services:services()) -> wh_services:services()).
+-type update_fun() :: fun((ne_binary(), ne_binary(), integer(), wh_services:services()) ->
+                                 wh_services:services()
+                                     ).
+
+-spec reconcile_foldl(ip_info(), wh_services:services(), get_fun(), update_fun()) ->
                              wh_services:services().
-reconcile_foldl({Type, Quantity}, Services) ->
-    OldQuantity = wh_services:updated_quantity(<<"ips">>, Type, Services),
-    wh_services:update(<<"ips">>
-                       ,Type
-                       ,OldQuantity + wh_util:to_integer(Quantity)
-                       ,Services
-                      ).
+reconcile_foldl({Type, Quantity}, Services, GetFun, UpdateFun) ->
+    OldQuantity = GetFun(?CATEGORY, Type, Services),
+    UpdateFun(?CATEGORY
+              ,Type
+              ,OldQuantity + wh_util:to_integer(Quantity)
+              ,Services
+             ).
