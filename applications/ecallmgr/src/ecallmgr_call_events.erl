45575c85af14f45194c04ee9d5cf0e4617ad7bb6
lazedo <luis.azedo@factorlusitano.com>
2015-08-25 12:32:47 +0100

    KAZOO-3795 add callee to channel record to preserve original callee

--- applications/ecallmgr/src/ecallmgr_call_events.erl
+++ applications/ecallmgr/src/ecallmgr_call_events.erl
@@ -631,12 +631,6 @@ generic_call_event_props(Props) ->
      ,{<<"Caller-ID-Name">>, props:get_first_defined([<<"variable_effective_caller_id_name">>
                                                       ,<<"Caller-Caller-ID-Name">>
                                                      ], Props)}
-     ,{<<"Callee-ID-Number">>, props:get_first_defined([<<"variable_effective_callee_id_number">>
-                                                        ,<<"Caller-Callee-ID-Number">>
-                                                       ], Props)}
-     ,{<<"Callee-ID-Name">>, props:get_first_defined([<<"variable_effective_callee_id_name">>
-                                                      ,<<"Caller-Callee-ID-Name">>
-                                                     ], Props)}
      ,{<<"Other-Leg-Direction">>, props:get_value(<<"Other-Leg-Direction">>, Props)}
      ,{<<"Other-Leg-Caller-ID-Name">>, props:get_value(<<"Other-Leg-Caller-ID-Name">>, Props)}
      ,{<<"Other-Leg-Caller-ID-Number">>, props:get_value(<<"Other-Leg-Caller-ID-Number">>, Props)}
@@ -651,8 +645,26 @@ generic_call_event_props(Props) ->
      ,{<<"To-Tag">>, props:get_value(<<"variable_sip_to_tag">>, Props)}
      ,{<<"Switch-URL">>, props:get_value(<<"Switch-URL">>, Props)}
      ,{<<"Switch-URI">>, props:get_value(<<"Switch-URI">>, Props)}
-     | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+     | callee_call_event_props(Props) ++ wh_api:default_headers(?APP_NAME, ?APP_VERSION)
     ].
+
+-spec callee_call_event_props(wh_proplist()) -> wh_proplist().
+callee_call_event_props(Props) ->
+    UUID = get_call_id(Props),
+    case wh_cache:peek_local(?ECALLMGR_GROUP_CACHE, {'channel', UUID}) of
+        {'ok', Channel} ->
+            [{<<"Callee-ID-Number">>, Channel#channel.callee_number}
+             ,{<<"Callee-ID-Name">>, Channel#channel.callee_name}
+            ];
+        _ ->
+            [{<<"Callee-ID-Number">>, props:get_first_defined([<<"variable_effective_callee_id_number">>
+                                                               ,<<"Caller-Callee-ID-Number">>
+                                                              ], Props)}
+             ,{<<"Callee-ID-Name">>, props:get_first_defined([<<"variable_effective_callee_id_name">>
+                                                              ,<<"Caller-Callee-ID-Name">>
+                                                             ], Props)}
+            ]
+    end.
 
 -spec publish_event(wh_proplist()) -> 'ok'.
 publish_event(Props) ->
