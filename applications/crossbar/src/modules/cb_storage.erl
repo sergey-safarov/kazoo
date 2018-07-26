a2fc55c464981ef0c8325c3d6ac85bc09323593b
Sergey Safarov <s.safarov@gmail.com>
2018-07-26 21:23:09 +0000

    Fixed creation of storage document with empty data

--- applications/crossbar/src/modules/cb_storage.erl
+++ applications/crossbar/src/modules/cb_storage.erl
@@ -425,6 +425,8 @@ error_must_validate_settings(Context) ->
 -spec validate_attachments_settings(kz_json:object()
                                    ,cb_context:context()
                                    ) -> cb_context:context().
+validate_attachments_settings('undefined', Context) ->
+    kz_json:foldl(fun validate_attachment_settings_fold/3, Context, kz_json:new());
 validate_attachments_settings(Attachments, Context) ->
     kz_json:foldl(fun validate_attachment_settings_fold/3, Context, Attachments).
 
